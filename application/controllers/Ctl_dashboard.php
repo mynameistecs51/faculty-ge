66d45e36b480021a976be455892b9c48ab25873b
chaiwat <mynameistecs51@gmail.com>
2016-11-17 14:20:44 +0700

    update project ทำ template ใหม่

--- application/controllers/Ctl_dashboard.php
+++ application/controllers/Ctl_dashboard.php
@@ -11,9 +11,17 @@ class Ctl_dashboard extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('index');
+		$TEXTTITLE = "ทั่วไป";
+		$PAGE = 'index';
+		$this->mainpage($TEXTTITLE);
+		$this->load->view($PAGE,$this->data);
 	}
 
+	public function mainpage($TEXTTITLE)
+	{
+		$this->data['header'] = $this->template->getHeader(base_url(),$TEXTTITLE);
+		$this->data['footer'] = $this->template->getFooter(base_url());
+	}
 }
 
 /* End of file Ctl_dashboard.php */
