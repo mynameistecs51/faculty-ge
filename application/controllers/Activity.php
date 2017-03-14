c710cffa2ee987bcd639c9c212efb8af80c348be
chaiwat <mynameistecs51@gmail.com>
2017-03-14 07:48:20 +0700

    update code

--- application/controllers/Activity.php
+++ application/controllers/Activity.php
@@ -22,16 +22,16 @@ class Activity extends CI_Controller {
 
 	public function mainpage($TEXTTITLE)
 	{
-		$this->data['header'] = $this->template->getHeader(base_url(),$TEXTTITLE);
-		$this->data['footer'] = $this->template->getFooter(base_url());
+		$this->data['header']     = $this->template->getHeader(base_url(),$TEXTTITLE);
+		$this->data['footer']     = $this->template->getFooter(base_url());
 		$this->data['controller'] = $this->ctl;
-		$this->data['url_add'] = base_url().$this->ctl.'/add/';
+		$this->data['url_add']    = base_url().$this->ctl.'/add/';
 	}
 
 	public function add()
 	{
 		$TEXTTITLE = "เพิ่มกิจกรรม";
-		$PAGENAME="addActivity";
+		$PAGENAME  = "addActivity";
 		$this->data["datenow"] =$this->datenow;
 		$this->mainpage($TEXTTITLE);
 		$this->load->view('/Activity/'.$PAGENAME,$this->data);
@@ -43,8 +43,8 @@ class Activity extends CI_Controller {
 		// 	parse_str($_POST['form'], $post);
 		// //$code= $this->getCode();
 		$data = array(
-			'title' => $this->input->post('title'),
-			'detail' =>  str_replace("\n", "<br>",$this->input->post('detail')),
+			'title'   => $this->input->post('title'),
+			'detail'  => str_replace("\n", "<br>",$this->input->post('detail')),
 			'picture' => $this->input->post('pict')
 			// $detail =  str_replace("\n", "<br>\n",$post['input_detail']),
 			// $picture = $_FILES['images'] //implode(',', $_FILES['images']['name'])
--- application/views/Activity/addActivity.php
+++ application/views/Activity/addActivity.php
@@ -1,36 +1,42 @@
- <!doctype html>
- <div class="row">
- 	<?php// echo form_open_multipart('#',' class="form-horizontal" role="form" ');?>
- 	<div class="form-group col-sm-6">
- 		<label for="input_title" class="col-sm-2 control-label">หัวข้อกิจกรรม </label>
- 		<div class="col-sm-10">
- 			<input type="text" class="form-control" id="input_title" name="input_title" value=""> <br/>
- 		</div>
- 		<label for="input_detail" class="col-sm-2 control-label">รายละเอียด </label>
- 		<div class="col-sm-10">
- 			<textarea class="form-control" rows="6" cols="50" id="input_detail" name="input_detail" ></textarea>
- 		</div>
- 	</div>
- 	<div class="form-group col-xs-6">
- 		<label for="input_picture" class="col-sm-2 control-label">รูปภาพ   </label>
- 		<div class="col-sm-8">
- 			<img id="show_pic" name="show_pic" src="<?php echo base_url().'assets/images/no-image.jpg';?>" alt=""  /><br/><br/>
- 			<input type="file" id="images" class="form-control" name="images[]" size="20" onchange="PreviewImage();" multiple=""/>
+ <!DOCTYPE html>
+ <html>
+ <head>
 
- 		</div>
- 	</div>
- 	<?php //echo form_close(); ?>
- </div>
- <script type="text/javascript">
- 	$(function(){
- 		saveData();
- 	});
+ </head>
+ <body>
 
- 	function saveData()
- 	{
- 		$('form#form').on('submit', function (e) {
- 			if (e.isDefaultPrevented()) {
- 				alert("ผิดพลาด : กรุณาตรวจสอบข้อมูลให้ถูกต้อง !");
+ 	<div class="row">
+
+ 		<div class="form-group col-sm-6">
+ 			<label for="input_title" class="col-sm-2 control-label">หัวข้อกิจกรรม </label>
+ 			<div class="col-sm-10">
+ 				<input type="text" class="form-control" id="input_title" name="input_title" value=""> <br/>
+ 			</div>
+ 			<label for="input_detail" class="col-sm-2 control-label">รายละเอียด </label>
+ 			<div class="col-sm-10">
+ 				<textarea class="form-control" rows="6" cols="50" id="input_detail" name="input_detail" ></textarea>
+ 			</div>
+ 		</div>
+ 		<div class="form-group col-xs-6">
+ 			<label for="input_picture" class="col-sm-2 control-label">รูปภาพ   </label>
+ 			<div class="col-sm-8">
+ 				<img id="show_pic" name="show_pic" src="<?php echo base_url().'assets/images/no-image.jpg';?>" alt=""  /><br/><br/>
+ 				<input type="file" id="images" class="form-control" name="images[]" size="20" multiple  />
+
+ 			</div>
+ 		</div>
+
+ 	</div>
+ 	<script type="text/javascript">
+ 		$(function(){
+ 			saveData();
+ 		});
+
+ 		function saveData()
+ 		{
+ 			$('form#form').on('submit', function (e) {
+ 				if (e.isDefaultPrevented()) {
+ 					alert("ผิดพลาด : กรุณาตรวจสอบข้อมูลให้ถูกต้อง !");
               // handle the invalid form...
             } else {
               // everything looks good!
@@ -56,6 +62,9 @@
 	              });
            }
          });
- 	}
+ 		}
 
- </script>
+ 	</script>
+
+ </body>
+ </html>
