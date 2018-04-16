caaf935f0c4ac777109bb301b5dc07f5230a4527
te_chaiwat <te.chaiwat@gmail.com>
2018-04-16 21:58:33 +0700

    update show link template

--- application/libraries/Template.php
+++ application/libraries/Template.php
@@ -8,116 +8,117 @@ class Template
 	public function __construct()
 	{
 		$this->ci =& get_instance();
+    $this->ci->load->model('mdl_link');
 		// $userID = $this->ci->session->userdata('userID');
 		// $userStatus = $this->ci->session->userdata('userStatus');
-	}
+  }
 
 
-	public function getHeader($base_url,$TEXTTITLE)
-	{
-		return '
-		<!DOCTYPE html>
-		<html>
-		<head>
+  public function getHeader($base_url,$TEXTTITLE)
+  {
+    return '
+    <!DOCTYPE html>
+    <html>
+    <head>
 
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="description" content="">
-		<meta name="author" content="">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-		<title> สำนักวิชาศึกษาทั่วไป</title>
+    <title> สำนักวิชาศึกษาทั่วไป</title>
 
-		<!-- Bootstrap Core CSS -->
-		<link href="'.$base_url.'assets/css/bootstrap.css" rel="stylesheet">
-		<!-- <link href="'.$base_url.'assets/css/bootstrap.min.css" rel="stylesheet">-->
+    <!-- Bootstrap Core CSS -->
+    <link href="'.$base_url.'assets/css/bootstrap.css" rel="stylesheet">
+    <!-- <link href="'.$base_url.'assets/css/bootstrap.min.css" rel="stylesheet">-->
 
-		<!-- Bootstrap DataTable CSS -->
-		<link href="'.$base_url.'assets/dataTable/css/jquery.dataTables.min.css" rel="stylesheet">
-		<!-- ./ End css -->
+    <!-- Bootstrap DataTable CSS -->
+    <link href="'.$base_url.'assets/dataTable/css/jquery.dataTables.min.css" rel="stylesheet">
+    <!-- ./ End css -->
 
-		<!-- Jquery UI -->
-		<link href="'.$base_url.'assets/css/jquery-ui.min.css" rel="stylesheet">
+    <!-- Jquery UI -->
+    <link href="'.$base_url.'assets/css/jquery-ui.min.css" rel="stylesheet">
 
-		<!-- Custom Fonts -->
-		<link href="'.$base_url.'assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!-- Custom Fonts -->
+    <link href="'.$base_url.'assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-		<script src="'.$base_url.'assets/js/jquery.min.js"></script>
-		<style >
-		@font-face {
-			font-family: TH_Charmonman;
-			src: url("'.$base_url.'assets/fonts/TH Charmonman.ttf");
-		}
+    <script src="'.$base_url.'assets/js/jquery.min.js"></script>
+    <style >
+    @font-face {
+     font-family: TH_Charmonman;
+     src: url("'.$base_url.'assets/fonts/TH Charmonman.ttf");
+   }
 
 			#brandner{
-		font-family: TH_Charmonman;
-		font-size: 30px;
-		padding-left:30px;
-		font-weight: bold;
-		color:white;
-	}
-	title{
-		font-family: TH_Charmonman;
-	}
-	body {
-		background:#efefef;
-		border : 1px solid #D9DEE4;
-	}
-	</style>
-	</head>
-	<body>
-	<!-- Navigation -->
-	<div class="col-sm-12"  >
-	<div class="row">
-	<nav class="navbar bg-primary " >
-	<div class="container-fluid">
-	<!-- Brand and toggle get grouped for better mobile display -->
-	<div class="navbar-header">
-	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" >
-	<span class="sr-only">Toggle navigation</span>
-	<span class="icon-bar" style="background-color:#fff;"></span>
-	<span class="icon-bar" style="background-color:#fff;"></span>
-	<span class="icon-bar" style="background-color:#fff;"></span>
-	</button>
-	<a class="navbar-brand" id="brandner" style="padding-top:20px;"> สำนักวิชาศึกษาทั่วไป</a>
-	</div>
+   font-family: TH_Charmonman;
+   font-size: 30px;
+   padding-left:30px;
+   font-weight: bold;
+   color:white;
+ }
+ title{
+  font-family: TH_Charmonman;
+}
+body {
+  background:#efefef;
+  border : 1px solid #D9DEE4;
+}
+</style>
+</head>
+<body>
+<!-- Navigation -->
+<div class="col-sm-12"  >
+<div class="row">
+<nav class="navbar bg-primary " >
+<div class="container-fluid">
+<!-- Brand and toggle get grouped for better mobile display -->
+<div class="navbar-header">
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" >
+<span class="sr-only">Toggle navigation</span>
+<span class="icon-bar" style="background-color:#fff;"></span>
+<span class="icon-bar" style="background-color:#fff;"></span>
+<span class="icon-bar" style="background-color:#fff;"></span>
+</button>
+<a class="navbar-brand" id="brandner" style="padding-top:20px;"> สำนักวิชาศึกษาทั่วไป</a>
+</div>
 
-	<!-- Collect the nav links, forms, and other content for toggling -->
-	<div class="collapse navbar-collapse " id="bs-example-navbar-collapse-1" >
-	'.$this->menu($base_url).'
-	</div><!-- /.navbar-collapse -->
-	</div><!-- /.container-fluid -->
-	</nav>
-	</div>
-	<!-- show body -->
-	<div class="row   style="background-color:none;padding: 20px;">
+<!-- Collect the nav links, forms, and other content for toggling -->
+<div class="collapse navbar-collapse " id="bs-example-navbar-collapse-1" >
+'.$this->menu($base_url).'
+</div><!-- /.navbar-collapse -->
+</div><!-- /.container-fluid -->
+</nav>
+</div>
+<!-- show body -->
+<div class="row   style="background-color:none;padding: 20px;">
 
-	<div class="content col-sm-3" >  <!--   menu left -->
-	<div class="panel panel-primary">
-	<div class="panel-heading"><i class="glyphicon glyphicon-home" aria-hidden="true"></i> เมนู</div>
-	<div class="panel-body ">
-	<div>'.anchor("/", "<h4><i class=\"glyphicon glyphicon-hand-right\"></i> หน้าแรก</h4>").'</div>
-	<div>'.anchor("/", "<h4><i class=\"glyphicon glyphicon-hand-right\"></i> ข่าวสาร</h4>").'</div>
-	<div>'.anchor("/", "<h4><i class=\"glyphicon glyphicon-hand-right\"></i> แหล่งทุน</h4>").'</div>
-	<div>'.anchor("/", "<h4><i class=\"glyphicon glyphicon-hand-right\"></i> สถาบันวิจัย มรภ.อุดรธานี</h4>").'</div>
-	<div>'.anchor("http://www.nrct.go.th/%E0%B8%AB%E0%B8%99%E0%B8%B2%E0%B8%AB%E0%B8%A5%E0%B8%81.aspx#.WUH8YpDyjIU", "<h4><i class=\"glyphicon glyphicon-hand-right\"></i> วช.</h4>",'target="_blank"').'</div>
-	<div>'.anchor("http://www.mua.go.th/ohec/", "<h4><i class=\"glyphicon glyphicon-hand-right\"></i> สกอ.</h4>",'target="_blank"').'</div>
-	</div>
-	</div>
-	</div> <!-- ./end content menu left -->
+<div class="content col-sm-3" >  <!--   menu left -->
+<div class="panel panel-primary">
+<div class="panel-heading"><i class="glyphicon glyphicon-home" aria-hidden="true"></i> เมนู</div>
+<div class="panel-body ">
+<div>'.anchor("/", "<h4><i class=\"glyphicon glyphicon-hand-right\"></i> หน้าแรก</h4>").'</div>
+<div>'.anchor("/", "<h4><i class=\"glyphicon glyphicon-hand-right\"></i> ข่าวสาร</h4>").'</div>
+<div>'.anchor("/", "<h4><i class=\"glyphicon glyphicon-hand-right\"></i> แหล่งทุน</h4>").'</div>
 
-	<!-- show body -->
-	<div class="content col-sm-9" >
+'.$this->link().'
 
-	<div class="row" >
-	<div class="col-sm-12" >
-	<h1 class="page-header" style="margin-top:0px;">
-	'. $TEXTTITLE.'
-	</h1>
-	</div>
-	</div>
-	<!-- /.row -->
-	';
+</div>
+</div>
+</div> <!-- ./end content menu left -->
+
+<!-- show body -->
+<div class="content col-sm-9" >
+
+<div class="row" >
+<div class="col-sm-12" >
+<h1 class="page-header" style="margin-top:0px;">
+'. $TEXTTITLE.'
+</h1>
+</div>
+</div>
+<!-- /.row -->
+';
 }
 
 public function getFooter($base_url)
@@ -160,49 +161,59 @@ public function menu($base_url)
 		<a href="#" class="dropdown-toggle" data-toggle="dropdown" style="color:#000;"><i class="fa fa-fw fa-gear" aria-hidden="true"></i> SETTING <b class="caret"></b></a>
 		<ul class="dropdown-menu ">
 		<li>
-			<a href="'.base_url().'index.php/Document"><i class="fa fa-fw fa-book" aria-hidden="true"></i> อัพโหลดงานวิจัย</a>
-		</li>
-		<li>
-			<a href="'.base_url().'index.php/Activity"><i class="fa fa-fw fa-image" aria-hidden="true"></i> อัพโหลดกิจกรรม</a>
-		</li>
-		<li>
-			<a href="'.base_url().'index.php/News"><i class="fa fa-newspaper-o" aria-hidden="true"></i> อัพโหลดข่าว</a>
-		</li>
-		<li>
-			<a href="'.base_url().'index.php/Fund"><i class="fa fa-fw fa-money" aria-hidden="true"></i> อัพโหลดแหล่งทุน</a>
-		</li>
-		<li>
-			<a href="'.base_url().'index.php/Link"><i class="fa fa-fw fa-link" aria-hidden="true"></i> อัพโหลดลิงค์</a>
-		</li>
-		<!--
-		<li class="col-sm-12" >
-		<a href="'.base_url().'index.php/management"><i class="fa fa-fw fa-gear" aria-hidden="true"></i> Settings</a>
-		</li>
-		-->
-		</ul>
-		</li>
-		';
-	}
+   <a href="'.base_url().'index.php/Document"><i class="fa fa-fw fa-book" aria-hidden="true"></i> อัพโหลดงานวิจัย</a>
+   </li>
+   <li>
+   <a href="'.base_url().'index.php/Activity"><i class="fa fa-fw fa-image" aria-hidden="true"></i> อัพโหลดกิจกรรม</a>
+   </li>
+   <li>
+   <a href="'.base_url().'index.php/News"><i class="fa fa-newspaper-o" aria-hidden="true"></i> อัพโหลดข่าว</a>
+   </li>
+   <li>
+   <a href="'.base_url().'index.php/Fund"><i class="fa fa-fw fa-money" aria-hidden="true"></i> อัพโหลดแหล่งทุน</a>
+   </li>
+   <li>
+   <a href="'.base_url().'index.php/Link"><i class="fa fa-fw fa-link" aria-hidden="true"></i> อัพโหลดลิงค์</a>
+   </li>
+   <!--
+   <li class="col-sm-12" >
+   <a href="'.base_url().'index.php/management"><i class="fa fa-fw fa-gear" aria-hidden="true"></i> Settings</a>
+   </li>
+   -->
+   </ul>
+   </li>
+   ';
+ }
 
-	if($this->ci->session->userdata('userID') != ''):
-		$html .= '
-		<li class="dropdown">
-		<a href="#" class="dropdown-toggle" data-toggle="dropdown"  style="color:#000;"> <i class="fa fa-user " aria-hidden="true"></i> '.$this->ci->session->userdata('username').' <b class="caret"></b></a>
-		<ul class="dropdown-menu ">
-		<li class="col-sm-12" >
-		<a href="'.base_url().'authen/logout"><i class="fa fa-fw fa-sign-out"></i> Logout</a>
-		</li>
-		</ul>
-		</li>
-		';
-	else:
-		$html .= '
-		<li > <a class="btn "  style="color:#000;font-weight:bold;" data-toggle="modal" data-target="#myModal"><i class="fa fa-fw fa-sign-in" aria-hidden="true"></i> Sign In</a>'.$this->loginForm().'</li>
-		</ul>
-		';
-	endif;
+ if($this->ci->session->userdata('userID') != ''):
+  $html .= '
+  <li class="dropdown">
+  <a href="#" class="dropdown-toggle" data-toggle="dropdown"  style="color:#000;"> <i class="fa fa-user " aria-hidden="true"></i> '.$this->ci->session->userdata('username').' <b class="caret"></b></a>
+  <ul class="dropdown-menu ">
+  <li class="col-sm-12" >
+  <a href="'.base_url().'authen/logout"><i class="fa fa-fw fa-sign-out"></i> Logout</a>
+  </li>
+  </ul>
+  </li>
+  ';
+else:
+  $html .= '
+  <li > <a class="btn "  style="color:#000;font-weight:bold;" data-toggle="modal" data-target="#myModal"><i class="fa fa-fw fa-sign-in" aria-hidden="true"></i> Sign In</a>'.$this->loginForm().'</li>
+  </ul>
+  ';
+endif;
 
-	return $html;
+return $html;
+}
+
+public function link()
+{
+  $html ='';
+  $link = $this->ci->mdl_link->getLink();
+  foreach ($link as $row) {
+    $html .= '<div>'.anchor($row->link_url, "<h4><i class=\"glyphicon glyphicon-hand-right\"></i> ".$row->link_name." </h4>",'target="_blank"').'</div>';
+  }
+  return $html;
 }
 
 function loginForm()
